994b8c2b16897f298d2b36025171043fd602f026
Ryan Cox <rcox@getblueiq.com>
2017-11-29 13:16:44 -0700

    Adding a third println and fixing the spelling of mic

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
-		System.out.println("Mike Drop");
+		System.out.println("Mic Drop");
+		
+		System.out.println("Oops tripped over the mic");
 	}
 
 }
